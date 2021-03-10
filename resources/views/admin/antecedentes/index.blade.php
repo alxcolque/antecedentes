93bdc68197c2482c55bd804f41f2e685af16bb81
unknown <a.colq.c@hotmail.com>
2021-03-10 10:18:08 -0400

    Cambio de logo y etiqueta tabla antecedentes

--- resources/views/admin/antecedentes/index.blade.php
+++ resources/views/admin/antecedentes/index.blade.php
@@ -4,10 +4,48 @@
 
 @section('content_header')
     <h1>Antecedentes</h1>
+    <button type="button" class="btn btn-success">Importar</button>
 @stop
 
 @section('content')
-    <p>Antecedentes.</p>
+    <div class="card">
+        <div class="card-body">
+            
+            <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">Codigo</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Caso</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>
+
+        
+
+        </div>
+    </div>
     
 @stop
 
